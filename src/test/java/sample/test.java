359afd950cf26252b797ac50bbab6a0e75621acf
manojbison9 <manojbison9@gmail.com>
2018-06-14 13:59:49 +0000

    new demo

---
+++ src/test/java/sample/test.java
@@ -0,0 +1,21 @@
+package sample;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class test {
+
+	 
+		@Test
+		public void simpltestpass(){
+			int a=1;
+			int b =2;
+			assertTrue( a + b == 3);
+			System.out.println("True");
+		}
+	
+
+}
+
